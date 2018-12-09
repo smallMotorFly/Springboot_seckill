3e7a510e16d447957c2a8a323cc2665522be312f
zhangbin <bin.zhang@juneyaokc.com>
2018-12-09 21:29:53 +0800

    mark

--- src/main/java/com/seckillproject/App.java
+++ src/main/java/com/seckillproject/App.java
@@ -1,13 +1,30 @@
 package com.seckillproject;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+//开启springboot的自动化配置
+@EnableAutoConfiguration
+@RestController
 public class App 
 {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world!";
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
+
+
 }
